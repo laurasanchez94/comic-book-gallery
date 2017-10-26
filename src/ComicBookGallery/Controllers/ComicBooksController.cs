b70b8998843de171f46a8693ee0a69237dc0f958
Laura Esmeralda Sanchez <laurasanchez94@outlook.com>
2017-10-26 12:56:35 -0500

    updated comic book detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,18 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.seriesTitle = "The amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! witnedd the final hours of Doctor Octopus life and his one , last great " +
+                        "act of revenge ! EventArgs if spidermans survives...<strong>peter parker/</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
             return View();
         }
     }
